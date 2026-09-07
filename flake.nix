7946fbb5937c52210bf7a0733a52e6b6ff263d93
Jared Ledvina <jared@techsmix.net>
2026-09-07 10:38:07 -0400

    [release] bump to v0.5.2-dev

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
 
           packages.default = pkgs.buildGoModule rec {
             pname = "seabird-nwwsio-plugin";
-            version = "0.5.1";
+            version = "0.5.2-dev";
 
             src = ./.;
 
